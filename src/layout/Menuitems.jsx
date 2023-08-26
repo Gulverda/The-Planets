a7a826909d637aceb0f2f6726b074cfeb23cb733
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2023-08-26 22:27:20 +0400

    First Commit

---
+++ src/layout/Menuitems.jsx
@@ -0,0 +1,44 @@
+const MenuItems = [
+    {
+        title: 'MERCURY',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'VENUS',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'EARTH',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'MARS',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'JUPITER',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'SATURN',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'URANUS',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'NEPTUNE',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
+
+export default MenuItems;
