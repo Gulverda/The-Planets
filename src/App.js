cf3a2c1a5c48e59ca29380cefdcfbba7ab699fbf
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2023-08-27 02:49:12 +0400

    Add Rounds in Navbar and Add Right Functions <3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React, { useState } from 'react';
 import Navbar from './layout/Navbar';
+import Content from './components/Content';
 import './App.css';
 
-
 function App() {
+  const [selectedPlanet, setSelectedPlanet] = useState(null);
+
   return (
     <div className="App">
       <div className="center">
-        <Navbar/>
+        <Navbar onSelectPlanet={setSelectedPlanet} />
+        <Content selectedPlanet={selectedPlanet} />
       </div>
     </div>
   );
--- src/layout/Menuitems.jsx
+++ src/layout/Menuitems.jsx
@@ -1,44 +1,61 @@
+import MercuryImage from './images/Mercury1.png';
+import VenusImage from './images/Venus1.png';
+import EarthImage from './images/Earth1.png';
+import NeptuneImage from './images/Neptune1.png';
+import MarsImage from './images/Mars1.png'
+import JupiterImage from './images/Jupiter1.png';
+import SaturnImage from './images/Saturn1.png';
+import UranusImage from './images/Uranus1.png';
+
 const MenuItems = [
-    {
-        title: 'MERCURY',
-        url: '#',
-        cName: 'nav-links'
-    },
-    {
-        title: 'VENUS',
-        url: '#',
-        cName: 'nav-links'
-    },
-    {
-        title: 'EARTH',
-        url: '#',
-        cName: 'nav-links'
-    },
-    {
-        title: 'MARS',
-        url: '#',
-        cName: 'nav-links'
-    },
-    {
-        title: 'JUPITER',
-        url: '#',
-        cName: 'nav-links'
-    },
-    {
-        title: 'SATURN',
-        url: '#',
-        cName: 'nav-links'
-    },
-    {
-        title: 'URANUS',
-        url: '#',
-        cName: 'nav-links'
-    },
-    {
-        title: 'NEPTUNE',
-        url: '#',
-        cName: 'nav-links'
-    }
-]
+  {
+    title: 'MERCURY',
+    url: '#',
+    cName: 'nav-links',
+    imgSrc: MercuryImage
+  },
+  {
+    title: 'VENUS',
+    url: '#',
+    cName: 'nav-links',
+    imgSrc: VenusImage // Use the imported module directly
+  },
+  {
+    title: 'EARTH',
+    url: '#',
+    cName: 'nav-links',
+    imgSrc: EarthImage
+  },
+  {
+    title: 'MARS',
+    url: '#',
+    cName: 'nav-links',
+    imgSrc: MarsImage
+  },
+  {
+    title: 'JUPITER',
+    url: '#',
+    cName: 'nav-links',
+    imgSrc: JupiterImage
+  },
+  {
+    title: 'SATURN',
+    url: '#',
+    cName: 'nav-links',
+    imgSrc: SaturnImage
+  },
+  {
+    title: 'URANUS',
+    url: '#',
+    cName: 'nav-links',
+    imgSrc: UranusImage
+  },
+  {
+    title: 'NEPTUNE',
+    url: '#',
+    cName: 'nav-links',
+    imgSrc: NeptuneImage // Use the imported module directly
+  }
+];
 
 export default MenuItems;
