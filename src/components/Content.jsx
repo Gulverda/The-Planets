52eb01f89241c8daafef83b8f7fed0e58fee6d9a
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2023-08-31 22:25:09 +0400

    wikipedia Links

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -42,7 +42,8 @@ function Content({ selectedPlanet }) {
             radius: "2,439.7 KM",
             temperature: "430°C",
             description1: "Mercury appears to have a solid silicate crust and mantle overlying a solid, iron sulfide outer core layer, a deeper liquid core layer, and a solid inner core. The planet's density is the second highest in the Solar System at 5.427 g/cm3 , only slightly less than Earth's density.",
-            description2: "Mercury's surface is similar in appearance to that of the Moon, showing extensive mare-like plains and heavy cratering, indicating that it has been geologically inactive for billions of years. It is more heterogeneous than either Mars's or the Moon’s."
+            description2: "Mercury's surface is similar in appearance to that of the Moon, showing extensive mare-like plains and heavy cratering, indicating that it has been geologically inactive for billions of years. It is more heterogeneous than either Mars's or the Moon’s.",
+            wikipediaURL: "https://en.wikipedia.org/wiki/Mercury_(planet)"
         },
         {
             id: 2,
@@ -56,7 +57,8 @@ function Content({ selectedPlanet }) {
             radius: "6,051.8 KM",
             temperature: "471°C",
             description1: "The similarity in size and density between Venus and Earth suggests they share a similar internal structure: a core, mantle, and crust. Like that of Earth, Venusian core is most likely at least partially liquid because the two planets have been cooling at about the same rate.",
-            description2: "Much of the Venusian surface appears to have been shaped by volcanic activity. Venus has several times as many volcanoes as Earth, and it has 167 large volcanoes that are over 100 km (60 mi) across. The only volcanic complex of this size on Earth is the Big Island of Hawaii."
+            description2: "Much of the Venusian surface appears to have been shaped by volcanic activity. Venus has several times as many volcanoes as Earth, and it has 167 large volcanoes that are over 100 km (60 mi) across. The only volcanic complex of this size on Earth is the Big Island of Hawaii.",
+            wikipediaURL: "https://en.wikipedia.org/wiki/Venus"
         },
         {
             id: 3,
@@ -70,7 +72,8 @@ function Content({ selectedPlanet }) {
             radius: "6,371 KM",
             temperature: "16°C",
             description1: "Earth's interior, like that of the other terrestrial planets, is divided into layers by their chemical or physical (rheological) properties. The outer layer is a chemically distinct silicate solid crust, which is underlain by a highly viscous solid mantle.",
-            description2: "The total surface area of Earth is about 510 million km2. The continental crust consists of lower density material such as the igneous rocks granite and andesite. Less common is basalt, a denser volcanic rock that is the primary constituent of the ocean floors."
+            description2: "The total surface area of Earth is about 510 million km2. The continental crust consists of lower density material such as the igneous rocks granite and andesite. Less common is basalt, a denser volcanic rock that is the primary constituent of the ocean floors.",
+            wikipediaURL: "https://en.wikipedia.org/wiki/Earth"
         },
         {
             id: 4,
@@ -84,7 +87,8 @@ function Content({ selectedPlanet }) {
             radius: "3,389.5 KM",
             temperature: "-28°C",
             description1: "Like Earth, Mars has differentiated into a dense metallic core overlaid by less dense materials. Scientists initially determined that the core is at least partially liquid. Current models of its interior imply a core consisting primarily of iron and nickel with about 16–17% sulfur.",
-            description2: "Mars is a terrestrial planet whose surface consists of minerals containing silicon and oxygen, metals, and other elements that typically make up rock. The surface is primarily composed of tholeiitic basalt, although parts are more silica-rich than typical basalt."
+            description2: "Mars is a terrestrial planet whose surface consists of minerals containing silicon and oxygen, metals, and other elements that typically make up rock. The surface is primarily composed of tholeiitic basalt, although parts are more silica-rich than typical basalt.",
+            wikipediaURL: "https://en.wikipedia.org/wiki/Mars"
         },
         {
             id: 5,
@@ -98,7 +102,8 @@ function Content({ selectedPlanet }) {
             radius: "69,911 KM",
             temperature: "-108°C",
             description1: "When the Juno arrived in 2016, it found that Jupiter has a very diffuse core that mixes into its mantle. A possible cause is an impact from a planet of about ten Earth masses a few million years after Jupiter's formation, which would have disrupted an originally solid Jovian core.",
-            description2: "The best known feature of Jupiter is the Great Red Spot, a persistent anticyclonic storm located 22° south of the equator. It is known to have existed since at least 1831, and possibly since 1665."
+            description2: "The best known feature of Jupiter is the Great Red Spot, a persistent anticyclonic storm located 22° south of the equator. It is known to have existed since at least 1831, and possibly since 1665.",
+            wikipediaURL: "https://en.wikipedia.org/wiki/Jupiter"
         },
         {
             id: 6,
@@ -112,7 +117,8 @@ function Content({ selectedPlanet }) {
             radius: "58,232 KM",
             temperature: "-138°C",
             description1: "Despite consisting mostly of hydrogen and helium, most of Saturn's mass is not in the gas phase, because hydrogen becomes a non-ideal liquid when the density is above 0.01 g/cm3, which is reached at a radius containing 99.9% of Saturn's mass.",
-            description2: "The outer atmosphere of Saturn contains 96.3% molecular hydrogen and 3.25% helium by volume. The planet's most famous feature is its prominent ring system, which is composed mostly of ice particles with a smaller amount of rocky debris and dust. "
+            description2: "The outer atmosphere of Saturn contains 96.3% molecular hydrogen and 3.25% helium by volume. The planet's most famous feature is its prominent ring system, which is composed mostly of ice particles with a smaller amount of rocky debris and dust.",
+            wikipediaURL: "https://en.wikipedia.org/wiki/Saturn"
         },
         {
             id: 7,
@@ -126,7 +132,8 @@ function Content({ selectedPlanet }) {
             radius: "25,362 KM",
             temperature: "-195°C",
             description1: "The standard model of Uranus's structure is that it consists of three layers: a rocky (silicate/iron–nickel) core in the centre, an icy mantle in the middle and an outer gaseous hydrogen/helium envelope. The core is relatively small, with a mass of only 0.55 Earth masses.",
-            description2: "The composition of Uranus's atmosphere is different from its bulk, consisting mainly of molecular hydrogen and helium. The helium molar fraction, i.e. the number of helium atoms per molecule of gas, is 0.15±0.03 in the upper troposphere."
+            description2: "The composition of Uranus's atmosphere is different from its bulk, consisting mainly of molecular hydrogen and helium. The helium molar fraction, i.e. the number of helium atoms per molecule of gas, is 0.15±0.03 in the upper troposphere.",
+            wikipediaURL: "https://en.wikipedia.org/wiki/Uranus"
         },
         {
             id: 8,
@@ -140,7 +147,8 @@ function Content({ selectedPlanet }) {
             radius: "24,622 KM",
             temperature: "-201°C",
             description1: "Neptune's internal structure resembles that of Uranus. Its atmosphere forms about 5% to 10% of its mass and extends perhaps 10% to 20% of the way towards the core. Increasing concentrations of methane, ammonia and water are found in the lower regions.",
-            description2: "Neptune's atmosphere is 80% hydrogen and 19% helium. A trace amount of methane is also present. Prominent absorption bands of methane exist at wavelengths above 600 nm, in the red and infrared portion of the spectrum."
+            description2: "Neptune's atmosphere is 80% hydrogen and 19% helium. A trace amount of methane is also present. Prominent absorption bands of methane exist at wavelengths above 600 nm, in the red and infrared portion of the spectrum.",
+            wikipediaURL: "https://en.wikipedia.org/wiki/Neptune"
         },
     ];
 
